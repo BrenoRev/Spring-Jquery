c52a125e83a91b17d026a19a2d10ff22dd1969f0
Breno Rev <silvabreno462@gmail.com>
2021-08-17 01:25:48 -0300

    Criando end-point de listar todos os usuários

--- src/main/java/com/brenodev/restapi/controller/principalController.java
+++ src/main/java/com/brenodev/restapi/controller/principalController.java
@@ -1,18 +1,43 @@
 package com.brenodev.restapi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brenodev.restapi.model.Usuario;
+import com.brenodev.restapi.services.UsuarioServices;
 
 @RestController
 public class principalController {
 
+	@Autowired
+	UsuarioServices usuarioService;
+	
 	@GetMapping("/{nome}")
 	@ResponseStatus(HttpStatus.OK)
 	public String greeting(@PathVariable String nome) {
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome(nome);
+		usuarioService.save(usuario);
+		
 		return "Bem vindo " + nome;
+	}
+	
+	@GetMapping("/listatodos")
+	@ResponseBody // Retorna os dados para o corpo da resposta
+	public ResponseEntity<List<Usuario>> listaUsuario(){
+		
+		List<Usuario> usuarios = usuarioService.findAll();
+		
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /* Retorna a lista em JSON */
 	}
 	
 }
--- src/main/java/com/brenodev/restapi/services/UsuarioServices.java
+++ src/main/java/com/brenodev/restapi/services/UsuarioServices.java
@@ -1,8 +1,11 @@
 package com.brenodev.restapi.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.brenodev.restapi.model.Usuario;
 import com.brenodev.restapi.repository.UsuarioRepository;
 
 @Service
@@ -10,4 +13,12 @@ public class UsuarioServices {
 
 	@Autowired
 	UsuarioRepository usuarioRepository;
+	
+	public Usuario save(Usuario usuario) {
+		return usuarioRepository.save(usuario);
+	}
+	
+	public List<Usuario> findAll(){
+		return usuarioRepository.findAll();
+	}
 }
