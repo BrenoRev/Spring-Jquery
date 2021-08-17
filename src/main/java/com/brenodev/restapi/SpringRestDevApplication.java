8edfaca6dcc75ba8a213f666a13ff2b34277860e
Breno Rev <silvabreno462@gmail.com>
2021-08-17 00:44:57 -0300

    Starting Project

---
+++ src/main/java/com/brenodev/restapi/SpringRestDevApplication.java
@@ -0,0 +1,13 @@
+package com.brenodev.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brenodev/restapi/SpringRestDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenodev.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
